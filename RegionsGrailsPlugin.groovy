2cb271525a0bd74fd8ec6fa2c0f22753fb45c077
Helen Manders Jones <realhelenmjones@gmail.com>
2022-08-11 16:19:43 +0100

    travis/nexus

--- RegionsGrailsPlugin.groovy
+++ RegionsGrailsPlugin.groovy
@@ -1,6 +1,7 @@
 class RegionsGrailsPlugin {
     // the plugin version
     def version = "2.5-NBN-PLUGIN-FFTF-0-0-SNAPSHOT"
+    def group = "au.org.ala"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
